03363d04a2b206ab488159cc4518c6c8b8a1c9c4
Bence <m.bence05@gmail.com>
2020-06-09 13:58:58 +0200

    pripojenie ku spring cloud config server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const node = require('./resources/bootstrap');
+const client = require("cloud-config-client");
 const bodyParser = require('body-parser');
 const express = require('express');
 
@@ -6,6 +7,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(node.server.port, () => {
+client.load({
+    endpoint: node.cloud.config.uri,
+    name: node.application.name,
+    profiles: node.profiles.active,
+    auth: {user: node.cloud.config.username, pass: node.cloud.config.password}
+}).then(config => {
+    config.bootstrap = node;
+
+    return app.listen(node.server.port);
+}).then(() => {
     console.log(`Server beží na porte ${node.server.port}`)
-});
+}).catch(console.error);
