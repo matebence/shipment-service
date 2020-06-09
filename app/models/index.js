eb23f4283c271448cf59d769bbee2e698e9fbf88
Bence <m.bence05@gmail.com>
2020-06-09 14:19:20 +0200

    zakladna struktura aplikacie

---
+++ app/models/index.js
@@ -0,0 +1,5 @@
+module.exports = (app, config) => {
+
+    const database = {};
+    module.exports = database;
+};
--- server.js
+++ server.js
@@ -23,8 +23,17 @@ client.load({
 }).then(config => {
     config.bootstrap = node;
 
+    require("./app/models")(app, config);
+
     require("./app/component/eureka.component")(app, config);
     require("./app/component/zipkin.component")(app, config);
+
+    require("./app/routes/auth.routes")(app, config);
+
+    require("./app/routes/shipments.routes")(app);
+    require("./app/routes/status.routes")(app);
+
+    require("./app/routes/errors.routes")(app);
 
     return app.listen(node.server.port);
 }).then(() => {
