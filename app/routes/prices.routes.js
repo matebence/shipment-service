acc8254bb884e06172471dbcf6ba9ba7b9fa7c31
Bence <m.bence05@gmail.com>
2020-06-28 18:23:23 +0200

    odstranenie create a delete method pre prices model

--- app/routes/prices.routes.js
+++ app/routes/prices.routes.js
@@ -2,10 +2,6 @@ module.exports = app => {
     const prices = require("../controllers/prices.controller");
     const router = require("express").Router();
 
-    router.post("/", prices.create.authorize, prices.create.checkBody, prices.create.validate, prices.create.inDatabase);
-
-    router.delete("/:id", prices.delete.authorize, prices.delete.validate, prices.delete.inDatabase);
-
     router.put("/:id", prices.update.authorize, prices.update.checkBody, prices.update.validate, prices.update.inDatabase);
 
     router.get("/:id", prices.get.authorize, prices.get.validate, prices.get.inDatabase);
